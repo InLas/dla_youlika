e005a8041c6e98ab526b95d7e6b2628809cc83d7
Сергей Сальников <73998785+InLas@users.noreply.github.com>
2022-04-07 17:10:20 +0300

    Add files via upload

---
+++ dla_youlika/js/script.js
@@ -0,0 +1,52 @@
+'use strict'
+
+
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const divka = document.querySelector('.div');
+
+
+function numMaker(numbers) {
+
+  let arr = numbers.split(' ');
+
+  let obj = Object.assign({}, arr);
+
+  for (let key in obj) {
+
+    init(obj[key]);
+
+  }
+
+}
+
+function init(num) {
+
+  let arr = num.split('-');
+
+  let preFinal = '';
+
+  for (let i = arr[0]; i <= arr[1]; i++) {
+
+    preFinal += i + ';';
+
+  }
+
+  let final = preFinal.substring(0, preFinal.length - 1);
+
+  adder(final);
+
+}
+
+function adder(final) {
+
+  let p = document.createElement('p');
+  p.className = 'total';
+  p.innerHTML = final;
+
+  divka.append(p);
+}
+
+button.addEventListener('click', () => {
+  numMaker(input.value);
+})
